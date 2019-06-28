35e1669d37fe9b8471f6c8df73bcdd0ebc6e99a3
Sandeep <sandeeep021@gmail.com>
2019-06-28 18:32:45 +0530

    burger app basic completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
-import './index.js';
-import Layout from './component/Layout/Layout';
-import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
-  
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
-  render(){
+  render () {
     return (
-      <div> 
-        <Layout>  
-        <BurgerBuilder> 
-        </BurgerBuilder>
+      <div>
+        <Layout>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
-  };
-} 
+  }
+}
 
 export default App;
