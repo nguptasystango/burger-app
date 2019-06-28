73482c65f79fa33a8563ad7898480ab9188b5d0c
Sandeep <sandeeep021@gmail.com>
2019-06-28 14:20:45 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './index.js';
+import Layout from './component/Layout/Layout';
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
+  
+class App extends Component {
+  render(){
+    return (
+      <div> 
+        <Layout>  
+        <BurgerBuilder> 
+        </BurgerBuilder>
+        </Layout>
+      </div>
+    );
+  };
+} 
+
+export default App;
---
+++ src/component/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 
+
+const  burgerIngredient = () => { 
+    return (
+      <div>
+        
+      </div>
+    )
+   
+}
+export default burgerIngredient;
